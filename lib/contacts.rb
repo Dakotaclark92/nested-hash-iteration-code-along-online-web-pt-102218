7a097037d0626ac1ff784c46577e0837a821eaec
Dakota Clark <bannaray95@gmail.com>
2018-11-05 01:54:19 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,16 +15,7 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts = {
-     "Jon Snow" => {
-       name: "Jon",
-       email: "jon_snow@thewall.we", 
-       favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-     },
-     "Freddy Mercury" => {
-       name: "Freddy",
-       email: "freddy@mercury.com",
-       favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-     }
-   }
+contacts.each do |person,
+contacts_details_hash|
+binding.pry
 end
